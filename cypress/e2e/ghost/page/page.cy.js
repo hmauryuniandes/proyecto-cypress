ff72f3260fbf5133872fc24248a0c5e5821a460c
SsantiagoRC <ds.rondon@uniandes.edu.co>
2023-05-05 23:51:42 -0500

    Editar pagina

--- cypress/e2e/ghost/page/page.cy.js
+++ cypress/e2e/ghost/page/page.cy.js
@@ -9,20 +9,21 @@ describe("Testing page creation", () => {
   let _loginObject;
   let _pageObject;
   let menuObject;
-  
-  
-
-  it("Test create new page", () => {
-    // GIVEN: usuario autenticado
+  beforeEach(()=>{
+    // GIVEN: usuario autenticado y navego hasta pages
     _siteObject = new Site("http://localhost:2368/ghost/#/signin");
     _siteObject.given_user_visit_ghost();
+
     _loginObject = new Login();
     _loginObject.when_user_enter_credentials_and_click_on_login();
 
-    // WHEN: usuario navega a la pantalla pages
     menuObject = new Menu();
     menuObject.when_user_navigate_to_pages();
-    // AND: usuario da click en el boton new page
+  });
+  
+
+  it("Test create new page", () => {
+    // WHEN: usuario da click en el boton new page
     _pageObject = new Page();
     _pageObject.when_user_click_on_new_page();
     // AND: usuario ingresa el titulo y contenido de la pagina
@@ -36,6 +37,15 @@ describe("Testing page creation", () => {
     _pageObject.then_page_was_published();
     
   });
+
+  it("Test edit page", ()=>{
+     // WHEN: usuario da click en editar pagina
+     _pageObject = new Page();
+     _pageObject.when_user_click_on_edit_page();
+     //THEN: la pagina fue modificada
+     _pageObject.then_page_was_modified();
+    
+  })
 
   after(() => {
     _pageObject = new Page();
--- cypress/e2e/ghost/page/page.js
+++ cypress/e2e/ghost/page/page.js
@@ -27,9 +27,6 @@ export class Page {
         cy.wait(100);
         cy.get('a[href="#/pages/"]').click();
         cy.wait(100);
-        /*<button class="gh-btn-editor gh-publish-back-button" title="Close" data-test-button="close-publish-flow" type="button">
-            <span><svg viewBox="0 0 16 16"><path d="M12.05 1.303a1 1 0 01-.02 1.414L6.597 8l5.433 5.283a1 1 0 01-1.394 1.434L4.756 9a1.4 1.4 0 01-.423-1 1.39 1.39 0 01.423-1l5.88-5.717a1 1 0 011.414.02z"></path></svg> Editor</span>
-        </button>*/
         cy.get("body > div.gh-app > div > main > section > section > div.gh-list-sticky > div.posts-list.gh-list.feature-memberAttribution > div:nth-child(1)").click();
       };
   
@@ -44,16 +41,34 @@ export class Page {
         })
     };
   
-    when_user_delete_current_post = () => {
-      cy.get("button.post-settings").click();
-      cy.wait(500);
-      cy.get("button.settings-menu-delete-button").click();
-      cy.wait(500);
-      cy.get(".modal-content").within(() => {
-        cy.get("button.gh-btn-red").click();
-      });
+    
+    when_user_click_on_edit_page = () =>{
+        cy.get("body > div.gh-app > div > main > section > section > div.gh-list-sticky > div.posts-list.gh-list.feature-memberAttribution > div:nth-child(1)").click();
+        cy.wait(100)
+        cy.get('textarea[placeholder="Page title"]').type(" Modificada");
+        cy.wait(500)
+        cy.get("button.gh-editor-save-trigger").click();
     };
 
+    then_page_was_modified = () =>{
+        cy.get(
+            "aside > article > .gh-notification-content > .gh-notification-title"
+          ).then(($title) => {
+            expect($title[0].innerText).to.equal("Updated");
+          });
+
+          cy.get('a[href="#/pages/"').click();
+    };
+
+    when_user_delete_current_post = () => {
+        cy.get("button.post-settings").click();
+        cy.wait(500);
+        cy.get("button.settings-menu-delete-button").click();
+        cy.wait(500);
+        cy.get(".modal-content").within(() => {
+          cy.get("button.gh-btn-red").click();
+        });
+      };
     delete_all_pages = () =>
     {
         cy.get('[role="menuitem"]').each(page => {
