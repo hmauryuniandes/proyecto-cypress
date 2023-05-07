0449d4f30b185c3b47fb210bed298f964e7fbd29
Humberto Maury <h.maury@uniandes.edu.co>
2023-05-07 14:37:40 -0500

    login escenario

--- cypress/e2e/ghost/login/login.js
+++ cypress/e2e/ghost/login/login.js
@@ -20,8 +20,8 @@ export class Login {
   }
 
   when_user_enter_credentials_and_click_on_login = () => {
-    this.username.type(username);
-    this.password.type(password);
+    this.username.clear().type(username);
+    this.password.clear().type(password);
     this.loginButton.click();
     cy.wait(1000);
   };
